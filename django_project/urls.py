cc15bad494b0bc9f9c3b7a0bc7e1cf72bfd4107c
Syed Asif <sydasif78@gmail.com>
2025-06-16 20:50:49 +0500

    change routes

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("blog.urls")),  # Include the blog app's URLs
+    path("register/", user_views.register_user, name="register"),
+    path("", include("blog.urls")),
 ]
