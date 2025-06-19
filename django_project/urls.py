6d84aadd36c01b95c9a5727b9ee7d14656709a5e
Syed Asif <sydasif78@gmail.com>
2025-06-19 09:51:20 +0500

    ruff reformat code

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path, include
+from django.urls import include, path
+
 from users import views as user_views
 
 urlpatterns = [
